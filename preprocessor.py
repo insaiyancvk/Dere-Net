f5cc464597f71c565523650ff0865211b3c746b9
insaiyancvk <cvamshik1@gmail.com>
2021-07-21 23:49:10 +0530

    update TODOs

---
+++ preprocessor.py
@@ -0,0 +1,37 @@
+'''
+TODO:
+
+1. Import necassary libraries
+2. Define all paths
+3. Define DataLoader 
+
+'''
+
+# required imports: torch, torchvision, transforms, dataloader, datasets
+
+import torch
+from torch.utils.data import DataLoader
+
+
+import torchvision
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+
+FOLDERS = {}
+
+class PreProcessor():
+
+    def __init__():
+        pass
+
+    def train_transformer():
+        pass
+
+    def test_transformer():
+        pass
+
+    def train_loader():
+        pass
+    
+    def test_loader():
+        pass
---
+++ train.py
@@ -0,0 +1,18 @@
+'''
+
+TODO:
+
+1. import stuff
+2. Load augmented data from dataloader
+3. Set all functions and constants (loss func, optimizer func, epochs, folders, device etc)
+4. Define the model
+5. Build a training loop
+    1. Move to GPU
+    2. Define the training function
+    3. Calculate the running loss and store the same in list and later convert to np array and save the np array.
+    4. Calculate the accuracy after every batch. Calculate the average of same and store the values in a list 
+       and later convert it to np array and save theh np array.
+    5. After every epoch, test the model loss and accuracy 
+       (by following the same steps as done for training ie, move to list, convert to np array, save the np array) with the test data.
+
+'''
